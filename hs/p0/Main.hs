ff1676db13918b70c7dbc0d07893747c85fb7406
Kevin Qi <kevinqiesq@gmail.com>
2017-10-06 07:30:41 -0500

    Added basic stats functions in p0[avg and others]

--- hs/p0/Main.hs
+++ hs/p0/Main.hs
@@ -1,3 +1,13 @@
 succc x = succ (succ x)
 
 fc x = product [1..x]
+
+factors x = [a | a <- [1..x], mod x a == 0]
+
+pq :: (Integral a) => [a] -> [Float]
+pq x = [(fromIntegral a) / (fromIntegral b) | a <- factors (last x), b <- factors (head x)]
+
+--syndiv :: (Integral a, Num b) => [a] -> b -> [b]
+syndiv poly fac = foldl (\x y -> x ++ [y + ((last x) * fac)]) [head poly] (tail poly)
+
+pqfacs x = [a | a <- pq x, last (syndiv x a) == 0.0]

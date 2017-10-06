32bec92450065bed4038c6a0aeac59cc39b54db6
Botron7 <Kevin Qi>
2017-10-06 17:25:32 -0500

    Fixed again

--- hs/p0/Main.hs
+++ hs/p0/Main.hs
@@ -2,12 +2,10 @@ succc x = succ (succ x)
 
 fc x = product [1..x]
 
-<<<<<<< HEAD
 factors c = [a | a <- [1..c], mod c a == 0]
 
 pqroots x = [(a, b)| a <- factors (last x), b <- factors (head x)]
-=======
-factors x = [a | a <- [1..x], mod x a == 0]
+
 
 pq :: (Integral a) => [a] -> [Float]
 pq x = [(fromIntegral a) / (fromIntegral b) | a <- factors (last x), b <- factors (head x)]
@@ -16,4 +14,3 @@ pq x = [(fromIntegral a) / (fromIntegral b) | a <- factors (last x), b <- factor
 syndiv poly fac = foldl (\x y -> x ++ [y + ((last x) * fac)]) [head poly] (tail poly)
 
 pqfacs x = [a | a <- pq x, last (syndiv x a) == 0.0]
->>>>>>> ff1676db13918b70c7dbc0d07893747c85fb7406

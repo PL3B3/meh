6e97f909693fce694591fca85ed0ff00cc3ba463
Botron7 <Kevin Qi>
2017-10-06 17:16:11 -0500

    Added xp, variance to statBlob.hs. Working on sqrt

--- hs/statBlob/statBlob.hs
+++ hs/statBlob/statBlob.hs
@@ -4,12 +4,29 @@ import System.IO
 import Data.List
 import System.Directory
 
+xp :: Float -> Int -> Float
+xp base power = product ((take power (repeat base)) ++ [1.0])
+
 avg :: [Float] -> Float 
 avg x = (sum x) / (fromIntegral  (length x))
 
+variance :: [Float] -> Float
+variance x = sum [xp (a - (avg x)) 2| a <- x]
+
+--sqrt :: Double -> Double
+--sqrt x = sqrt x (x / 2) .00001
+--sqrt x guess acc
+--  |done = guess
+--  |otherwise = sqrt x guess2 acc
+--  where
+--    done = abs (guess * guess - x) < (acc * x)
+--    guess2 = (guess + x / guess) / 2
+    
+
+
 blobSort :: Ord a => [a] -> [a]
 blobSort [] = []
-blobSort (x:xs) = (sort under) ++ [x] ++ (sort over)
+blobSort (x:xs) = (blobSort under) ++ [x] ++ (blobSort over)
   where
     under = filter (< x) xs
     over = filter (>= x) xs

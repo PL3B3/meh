ee4b0ac3383f86ca707fb22475189fb1a1d0d087
HR MCMASTER <hr@debian.520arabella.net>
2018-01-01 18:08:15 -0500

    Updated Probability library. Module Jimbo uses String String variable representation called Snap. Working on basic Snap-to-float probability functions

--- hs/plumboblios/src/Jimbo.hs
+++ hs/plumboblios/src/Jimbo.hs
@@ -1,5 +1,5 @@
 module Jimbo
-  ( Probability
+  ( --Probability
 --  , complement
 --  , intersect
 --  , condition
@@ -11,50 +11,51 @@ module Jimbo
   ) where
 
 import qualified Data.Char as Chr
+import Data.List
+import qualified Data.Map as M
+import qualified Data.List as L
 
-data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday deriving (Eq, Ord, Show, Read, Bounded, Enum)
+data Snap = Snap [(String, String)] deriving (Show, Eq, Read)
+--data Frame = 
+testSnap = Snap [("int","4"), ("float","3.7"),("string","\"heyo\""),("intlist","[2,3,4]")]
 
-next :: (Enum a, Bounded a) => a -> a
---next e = eList !! rem (fromEnum e + 1 + eLength) eLength
-  --where eList = enumFrom (asTypeOf minBound e)
-    --    eLength = length eList
-next e
-  | fromEnum e == (length (enumFrom (asTypeOf minBound e))) - 1 = toEnum 0
-  | otherwise = succ e
+--data Checknet = [(String, (a -> Bool))]
 
-prev :: (Enum a, Bounded a) => a -> a
-prev e
-  | fromEnum e == 0 = toEnum $ (length (enumFrom (asTypeOf minBound e))) - 1
-  | otherwise = pred e
+stringify :: (Show a) => a -> String
+stringify = show
 
+singleSnap :: (Show a, Read a) => String -> a -> Snap
+singleSnap str dat = Snap [(str, show dat)]
+
+makeFrame :: (Show a, Read a) => String -> a -> (String, String)
+makeFrame str dat = (str, show dat)
+
+appendFrame :: (String, String) -> Snap -> Snap
+appendFrame frame (Snap g) = Snap (frame:g) 
+
+findFrame :: Snap -> String -> Maybe (String, String)
+findFrame (Snap (x:xs)) str
+  | fst x == str = Just x
+  | xs == [] = Nothing
+  | otherwise = findFrame (Snap xs) str
+
+maybeToReal :: Maybe a -> a
+maybeToReal (Just a) = a
+
+evalToBool :: (Show a, Read a) => Maybe (String, String) -> (a -> Bool) -> Maybe Bool
+evalToBool frame func 
+  | frame == Nothing = Nothing
+  | otherwise = Just (func (read $ snd $ maybeToReal frame))
+
+snapToBool :: (Show a, Read a) => Snap -> String -> (a -> Bool) -> Maybe Bool
+snapToBool snap str func = evalToBool (findFrame snap str) func
+
+--needs to handle cases where snapToBool yields Nothing
+--change to satisfactory / number of snaps containing the frame
+portionTrue :: (Show a, Read a) => [Snap] -> String -> (a -> Bool) -> Float
+portionTrue snapList str func = L.genericLength satisfecho / L.genericLength snapList
+  where satisfecho = filter (\x -> maybeToReal $ snapToBool x str func) snapList
+          | 
 {-
-val is the numerical value of the probability, like 50 percent chance of bla bla
-evt is the value of the state of the variable in the environment that we're looking at. Like we want the probabiltiy of a coin landing on heads. The environment has a coin which has a boolean true or false aka heads or tails. Our probability data thing is a Probability Bool where the EVT is TRUE, we're looking for the TRUE value of the VAR which is the coin in the environment
-var is the var we're examining: Coin for coin flip. Temperature for whatev problem with clim8 change but it's a hoax so don't sweat it
-wld is the current state of the boi
--}
-data Probability a = Probability {
-  val :: Double deriving Ord,
-  var :: Variable a,
-  evt :: a,
-  wld :: Environment
-                  } deriving (Show, Eq, Read)
 
-data PDist a = PDist [Probability a] deriving (Show, Eq, Read)
-
-data Environment = Environment ([Variable Int], [Variable Bool], [Variable Float]) deriving (Show, Eq, Read)
-
-data Variable a = Variable {
-  nam :: Char,
-  stt :: a
-                  } deriving (Show, Eq, Read)
-{-
-complement :: Probability -> Probability
-complement (Probability v e c) = Probability (1.0 - v) switch_e c
-  where switch_e
-          | Chr.isLower e = Chr.toUpper e
-          | otherwise = Chr.toLower e
-
-intersect :: Probability -> Probability -> Probability
-intersect (Probability baseValue baseEvent baseCond) (Probability newValue newEvent newCond) = Probability (baseValue * newValue) ([baseEvent] ++ " && " ++ [newEvent]) (baseCond ++ newCond)
 -}
--- hs/plumboblios/src/Lib.hs
+++ hs/plumboblios/src/Lib.hs
@@ -4,3 +4,35 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+{-
+data Elist = Elist [Environment] deriving (Show, Eq, Read)
+
+data Environment = Environment [Variable Int] [Variable Bool] [Variable Float] deriving (Show, Eq, Read)
+
+data Variable a = Variable {
+  nam :: String,
+  val :: a
+                  } deriving (Show, Eq, Read)
+
+testList = Elist [Environment [Variable {nam = n, val = v}] [Variable {nam = m, val = b}] [] | n <- ["jebVotes"], v <- [400], m <- ["jebWon"], b <- [True]]
+
+numGood :: (Num a, Fractional a) => Elist -> (Environment -> Bool) -> (Environment -> Bool) -> (a, a, a)
+numGood (Elist g) f c = (good, total, good / total)
+  where good = fromIntegral (length $ filter (\x -> (f x) && (c x)) g)
+        total = fromIntegral (length $ filter c g)
+
+pDouble :: (Num a, Fractional a) => Elist -> (Environment -> Bool) -> a
+pDouble (Elist a) f = numGood (Elist a) f / (fromIntegral (length a))
+
+complement :: Probability -> Probability
+complement (Probability v e c) = Probability (1.0 - v) switch_e c
+  where switch_e
+          | Chr.isLower e = Chr.toUpper e
+          | otherwise = Chr.toLower e
+
+intersect :: Probability -> Probability -> Probability
+intersect (Probability baseValue baseEvent baseCond) (Probability newValue newEvent newCond) = Probability (baseValue * newValue) ([baseEvent] ++ " && " ++ [newEvent]) (baseCond ++ newCond)
+
+-}

36ea1c277389bcfa8ba970d38a292252f1f68838
PL3B3 <kevinqiesq@gmail.com>
2018-01-09 18:37:33 -0600

    Updated Snek with new verification functions for legit bf code. Also updated plumboblios-posterior with a statistical difference function for proportions

--- hs/plumboblios/src/Posterior.hs
+++ hs/plumboblios/src/Posterior.hs
@@ -21,14 +21,17 @@ data PDist = PDist [(String, (String -> Bool))] [Snap]
 
 --calcContinuousPDist
 
-pooledProbability :: (Num a) => Double -> Double -> a -> a -> Double
+pooledProbability :: Double -> Double -> Double -> Double -> Double
 pooledProbability p1 p2 n1 n2 = (p1 * n1 + p2 * n2) / (n1 + n2)
 
-stdError :: (Num a) => Double -> Double -> a -> a -> Double
-stdError p1 p2 n1 n2 = 
+stdError :: Double -> Double -> Double -> Double -> Double
+stdError p1 p2 n1 n2 = (p1 - p2) / (sqrt $ p * (complement p) * ((1.0 / n1) + (1.0 / n2)))
+    where p = pooledProbability p1 p2 n1 n2
 
+{-
 stdErrorPooled :: Double -> 
 stdErrorPooled proportion 
 
 sigDiffProp :: (Num a, Fractional a) => (Double-> Double -> Bool) -> (Double -> Double -> Bool) -> Double -> Double -> a -> a -> a
 sigDiffProp null alt p1 p2 n1 n2 = 
+-}

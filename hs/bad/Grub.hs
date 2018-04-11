1592d31ac43abc95e289b001b9a9606c855d1037
PL3B3 <kevinqiesq@gmail.com>
2018-04-10 23:34:14 -0500

    //Grub *Modified 	Added MetaFuck, which takes a string of Brainfuck and applies 	that to Brainfuck. For example, regular Brainfuck symbol "+" 	applies to an integer and raises it by one. MetaFuck symbol "+" 	applies to a Brainfuck symbol like "[" and raises it to "]" (The 	order is defined by me) -- MetaFuck will be used to modify 	functions generated in BrainFuck

--- hs/bad/Grub.hs
+++ hs/bad/Grub.hs
@@ -13,3 +13,24 @@ main = do
 costFunc :: [L.Tape a] -> (L.Tape a -> L.Tape a) -> (L.Tape a -> L.Tape a -> Int) -> ([Int] -> Float) -> Float
 costFunc testList funcToTest errorFunc cumeFunc = cumeFunc $ zipWith errorFunc testList (map funcToTest testList)
 
+evalMetaFuck :: String -> String -> String
+evalMetaFuck metaStr bFuckStr = foldl (\x y -> run y x) '0':bFuckStr metaStr
+    where run '+' str 
+            | str !! dex == ',' = str
+            | otherwise = (fst $ splitAt dex str) ++ (succc (str !! dex) "+-<>[].,") ++ (snd $ splitAt dex str)
+                where dex = succ $ read (head str) :: Int
+                      succc elem list = head $ tail $ dropWhile (/=elem) list  
+          run '-' str
+            | str !! dex == '+' = str
+            | otherwise = (fst $ splitAt dex str) ++ (antisuccc (str !! dex) "+-<>[].,") ++ (snd $ splitAt dex str)
+                where dex = succ $ read (head str) :: Int
+                      antisuccc elem list = last $ takeWhile (/=elem) list
+          run '<' str = (succ (read (head str) :: Int)):str
+          run '>' str = (pred (read (head str) :: Int)):str
+          run '[' str = 
+          run ']' str =
+          run '.' str =
+          run ',' str =  
+
+seekStringLoopL
+seekStringLoopR
--- hs/plumboblios/src/Chimbo.hs
+++ hs/plumboblios/src/Chimbo.hs
@@ -1,6 +1,10 @@
 --Q!, 3-27-18
 --Poorly Designed AI
 
+module Chimbo (testTech) where
+
 import Bfuck
+
+
 
 

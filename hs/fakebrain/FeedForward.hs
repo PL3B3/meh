ce7e1de7d25a20d3818eb33f9320eb50d7c60e12
PL3B3 <kevinqiesq@gmail.com>
2018-06-11 11:37:07 -0500

    Made some bigfunc

--- hs/fakebrain/FeedForward.hs
+++ hs/fakebrain/FeedForward.hs
@@ -10,7 +10,9 @@ import MatrixMaths
 import System.Random
 
 --Neural network datatype: Array of weights and biases
-data Net = Net [Matrix Double] [[Double]]
+data Net = Net [(Matrix Double, [Double], Activation)] deriving (Show, Eq)
+
+data Activation = SIG | TANH deriving (Show, Eq)
 
 type Training = [(Layer, Layer)] 
 
@@ -23,7 +25,19 @@ cost net training = sum evaluated
           compareFloatLists a b = sum $ zipWith (\x y -> (x - y) ^2) a b
           evaluated = foldl (\x y -> (compareFloatLists (last $ layers $ newNetwork (fst y)) (snd y)):x) [] training
 
---backPropagate :: Network -> Training -> Network
+activation :: Activation -> (Double -> Double)
+activation a = case a of
+  | SIG -> (\x -> 1.0 / (1.0 + (exp (-x))))
+  | TANH -> (\x -> (2.0 / (1.0 + (exp ((-2) * x)))) - 1.0)
 
-=======
->>>>>>> dbc4a742da1b77f9870096d7113a541be6abf6eb
+activationDeriv :: Activation -> (Double -> Double)
+activationDeriv a = case a of
+  
+
+calcMatrix :: Net -> [Double] -> [[Double]]
+calcMatrix (Net layers) inputs = foldl (\x@(x:xs) y@(weights,biases,activ) -> (activation activ $ calc x weights biases) ++ xs ++ [calc x weights biases]) [inputs] layers
+  where calc a w b = zipWith (+) (matrixByVec w a) b
+
+
+
+--backPropagate :: Network -> Training -> Network
